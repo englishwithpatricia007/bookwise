47a00afe1a445d361797d5cd125f0bdf0ff21af8
Patricia Nascimento <englishwithpatricia007@gmail.com>
2025-08-01 14:12:29 -0300

    super globais

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-// index.php
-$livros =[
-    ['id' => 1, 'titulo' => 'Senhor dos Aneis', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'é uma obra épica de fantasia escrita por J.R.R. Tolkien, ambientada na Terra-média, um mundo fictício cheio de diversas raças como Hobbits, Elfos, Anões, Humanos e criaturas malignas como Orcs. A história gira em torno da jornada de Frodo Bolseiro para destruir o Um Anel, forjado pelo Senhor Sombrio, Sauron, na Montanha da Perdição, o único lugar onde ele pode ser desfeito.', 'avaliacoes' => 3],
-    ['id' => 2, 'titulo' => 'The Twelve Tribes of Hattie', 'autor' => 'Ayana Mathis', 'descricao' => 'The Twelve Tribes of Hattie, by Ayana Mathis, tells the story of Hattie Shepherd and her descendants, focusing on her children and a granddaughter, who are collectively seen as her "twelve tribes".', 'avaliacoes' => 5],
-    ['id' => 3, 'titulo' => 'Como dizer tudo em inglês', 'autor' => 'Ron Martinez', 'descricao' => 'Lorem ipsum', 'avaliacoes' => 4],
-];
+    require 'dados.php';
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
--- livro.php
+++ livro.php
@@ -1,6 +1,13 @@
 <?php
-    // var_dump($_REQUEST['id']);
-    $id = $_REQUEST['id'];
+require 'dados.php';
+
+$id = $_REQUEST['id'];
+$filtrado = array_filter($livros, fn($livro) => $livro['id'] == $id);
+// echo "<pre>";
+$livro = array_pop($filtrado);
+// var_dump($livro);
+// echo "</pre>";
+// echo json_encode($livro);
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +21,7 @@
 
 <body class="bg-stone-950 text-stone-200">
     <!-- <h1 class="text-3xl font-bold underline"> Hello world! </h1> -->
-     <header class="bg-stone-900 shadow-lg">
+    <header class="bg-stone-900 shadow-lg">
         <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
             <div class="font-bold text-xl tracking-wide">Book Wise</div>
             <ul class="flex space-x-4 font-bold">
@@ -25,10 +32,24 @@
                 <li><a href="/login.php" class="hover:underline">Fazer Login</a></li>
             </ul>
         </nav>
-     </header>
+    </header>
 
     <main class="mx-auto max-w-screen-lg space-y-4 p-4">
-        ID do Livro: <?=$id?>
+        <?=$livro['titulo']?>
+        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+            <div class="flex">
+                <div class="w-1/3">imagem</div>
+                <div class="space-y-1">
+                    <a href="/livro.php?id=<?= $livro['id'] ?>"
+                        class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+                    <div class="text-xs italic"><?= $livro['autor'] ?></div>
+                    <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 avaliações)</div>
+                </div>
+            </div>
+            <div class="text-sm mt-2">
+                <?= $livro['descricao'] ?>
+            </div>
+        </div>
     </main>
 
 
