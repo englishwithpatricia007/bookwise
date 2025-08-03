028949d539dc2e12408a9be7443b0b09f1912903
Patricia Nascimento <englishwithpatricia007@gmail.com>
2025-08-03 13:58:54 -0300

    SQL Injection

--- models/Livro.php
+++ models/Livro.php
@@ -5,19 +5,22 @@ class Livro {
     public $titulo;
     public $autor;
     public $descricao;
+    public $usuario_id;
 
-    public function __construct($id, $titulo, $autor, $descricao) {
+    public function __construct($id, $titulo, $autor, $descricao, $usuario_id) {
         $this->id = $id;
         $this->titulo = $titulo;
         $this->autor = $autor;
         $this->descricao = $descricao;
+        $this->usuario_id = $usuario_id;
     }
 
     public static function make($data) {
         $obj = new Livro($data['id'],
             $data['titulo'],
             $data['autor'],
-            $data['descricao']);
+            $data['descricao'],
+            usuario_id: $data['usuario_id']);
         return $obj;
     }
     
--- views/index.view.php
+++ views/index.view.php
@@ -11,7 +11,7 @@
     <?php foreach($livros as $livro): ?>
         <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
             <div class="flex">
-                <div class="w-1/3">Imagem</div>
+                <div class="w-1/3"><?=$livro->usuario_id?></div>
                 <div class="space-y-1">
                     <a href="/livro?id=<?= $livro->id ?>" class="font-semibold hover:underline"><?= $livro->titulo ?></a>
                     <div class="text-xs italic"><?= $livro->autor ?></div>
