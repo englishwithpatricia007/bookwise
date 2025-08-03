26b3e83e343f75b87adc55bc42d4558520766234
Patricia Nascimento <englishwithpatricia007@gmail.com>
2025-08-03 19:28:23 -0300

    deixando a busca mais genérica

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -2,6 +2,12 @@
 
 $pesquisar = $_REQUEST['pesquisar'] ?? '';
 
-$livros = (new DB)->livros($pesquisar);
+$livros = (new DB)->query(
+    "SELECT * FROM livros where titulo like :filtro",
+    Livro::class, 
+    ["filtro"=> "%$pesquisar%"])->fetchAll();
+
+// dd($livros);
+//livros($pesquisar);
 
 view('index', compact('livros'));
--- database.php
+++ database.php
@@ -7,31 +7,13 @@ class DB
         $this->db = new PDO('sqlite:database.sqlite');
     }
 
-    public function livros($pesquisa = null)
-    {
-        $prepare = $this->db->prepare(
-             "SELECT * FROM livros where 
-             usuario_id = 1 and titulo like :pesquisa"
-         );
-
-        $prepare->bindValue(':pesquisa', "%$pesquisa%");
-        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
-
-        $prepare->execute();
-
-        return $prepare->fetchAll();
+    public function query($query, $class = null, $params = []){
+        $prepare = $this->db->prepare($query);
+        if ($class) {
+            $prepare->setFetchMode(PDO::FETCH_CLASS, $class);
+        }   
+        $prepare->execute($params);
+        return $prepare;
     }
 
-    public function livro($id)
-    {
-        $prepare = $this->db->prepare(
-            "SELECT * FROM livros WHERE id = :id"
-        );
-        $prepare->bindParam(':id', $id);
-        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
-
-        $prepare->execute();
-
-        return $prepare->fetch();
-    }
 }
