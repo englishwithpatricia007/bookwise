6691983d1c8fbec1443ced1ff2f603de5cbe83d2
Patricia Nascimento <englishwithpatricia007@gmail.com>
2025-08-03 21:01:00 -0300

    login layout

--- views/index.view.php
+++ views/index.view.php
@@ -1,18 +1,15 @@
 <form class="w-full flex space-x-2 mt-6">
-    <input 
-        type="text" 
-        name="pesquisar"
+    <input type="text" name="pesquisar"
         class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"
-        placeholder="Pesquisar..."
-    >
+        placeholder="Pesquisar..." />
     <button type="submit">🔎</button>
 </form>
 
 <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-    <?php foreach($livros as $livro): ?>
+    <?php foreach ($livros as $livro): ?>
         <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
             <div class="flex">
-                <div class="w-1/3"><?=$livro->usuario_id?></div>
+                <div class="w-1/3"><?= $livro->usuario_id ?></div>
                 <div class="space-y-1">
                     <a href="/livro?id=<?= $livro->id ?>" class="font-semibold hover:underline"><?= $livro->titulo ?></a>
                     <div class="text-xs italic"><?= $livro->autor ?></div>
